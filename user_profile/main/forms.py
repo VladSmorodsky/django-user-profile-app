b02715af5053462e04fa149deddbec99a5136104
Vlad <vlad.zah@ukr.net>
2025-03-20 22:31:32 +0200

    DPA-2 Implemented register form

--- user_profile/main/forms.py
+++ user_profile/main/forms.py
@@ -2,38 +2,66 @@ import re
 from typing import Union, Any
 
 from django import forms
+from django.contrib.auth.models import User
+
+from main.models import UserProfile
 
 
-def validate_password(value: str) -> None:
-    """
-    Validate a password format.
-    :param value:
-    :return:
-    """
-    if re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[@#$%&_]).{8,}$', value) is None:
+def validate_password(password: str) -> None:
+    if re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[@#$%&_]).{8,}$', password) is None:
         raise forms.ValidationError('Password must contain symbols: a-z, A-Z, 0-9, @#$%&_')
 
 
-class RegistrationForm(forms.Form):
+class RegistrationForm(forms.ModelForm):
     """
     Form for registering a new user
     """
-    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     password = forms.CharField(label='Password',
                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
                                validators=[validate_password])
     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(
-        attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}), validators=[validate_password])
+        attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
 
-    def clean_password(self) -> Union[dict[str, Any] | None]:
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+        }
+
+    def clean_name(self) -> str:
+        """
+        Check if name is already taken
+        :return:
+        """
+        name = self.cleaned_data['name']
+        existed_usernames = User.objects.filter(username=name)
+
+        if existed_usernames.count():
+            raise forms.ValidationError('Username already exists')
+        return name
+
+    def clean_email(self) -> str:
+        """
+        Check if email is already taken
+        :return:
+        """
+        email = self.cleaned_data['email']
+        existed_emails = User.objects.filter(email=email)
+        if existed_emails.count():
+            raise forms.ValidationError('Email already exists')
+        return email
+
+    def clean(self) -> Union[dict[str, Any] | None]:
         """
         Checks that the password is correct.
         :return:
         """
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
-        password2 = cleaned_data.get("password2")
-        if password and password2 and password != password2:
-            raise forms.ValidationError("Passwords don't match")
+        password_confirmed = cleaned_data.get("password2")
+        if password and password_confirmed and password != password_confirmed:
+            self.add_error("password2", "Passwords do not match.")
         return cleaned_data
--- user_profile/main/views.py
+++ user_profile/main/views.py
@@ -1,20 +1,38 @@
+from django.contrib.auth import login
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
 from main.forms import RegistrationForm
+from main.models import UserProfile
 
 
 # Create your views here.
+
+def home(request: HttpRequest) -> HttpResponse:
+    """
+    Home page
+    :param request:
+    :return:
+    """
+    return render(request, "main/home.html")
+
+
 def register(request: HttpRequest) -> HttpResponse:
     """
     Register a new user
     :param request:
     :return:
     """
+    if request.user.is_authenticated:
+        return redirect("home")
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            # form.save()
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            UserProfile.objects.create(user=user)
+            login(request, user)
             return redirect('home')
     else:
         form = RegistrationForm()
